3e9a0493115ce8be3801ca0f40030dbd85af0f2b
GSarangdeveloper <118855339+GSarangdeveloper@users.noreply.github.com>
2022-12-25 20:30:31 -0600

    Update KeyvaultApplication.java

--- src/main/java/com/contoso/keyvault/KeyvaultApplication.java
+++ src/main/java/com/contoso/keyvault/KeyvaultApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 @RestController
 public class KeyvaultApplication implements CommandLineRunner {
-    @Value("${DBUSER3}")
+    @Value("${Testing}")
     private String connectionString ;
 
     public static void main(String[] args) {
